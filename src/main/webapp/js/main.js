e4b124f885f75f70e3cae8af6bb63028106dd840
tra1103 <thutrathao@gmail.com>
2021-08-09 22:16:15 +0700

    Khoi Tao

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -10,7 +10,7 @@
 <!-- The core Firebase JS SDK is always required and must be listed first -->
 /*<script src="https://www.gstatic.com/firebasejs/8.9.0/firebase-app.js"></script>
 
-/* TODO: Add SDKs for Firebase products that you want to use
+
 https://firebase.google.com/docs/web/setup#available-libraries -->
     <script
 src="https://www.gstatic.com/firebasejs/8.9.0/firebase-analytics.js">*/
@@ -19,18 +19,18 @@ src="https://www.gstatic.com/firebasejs/8.9.0/firebase-analytics.js">*/
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     var firebaseConfig = {
-    apiKey: "AIzaSyArYCA14-VbksnUswWrqMmdPs5qgNxhn3I",
-    authDomain: "mflix-mflix2021.firebaseapp.com",
-    projectId: "mflix-mflix2021",
-    storageBucket: "mflix-mflix2021.appspot.com",
-    messagingSenderId: "519775413902",
-    appId: "1:519775413902:web:f2128094e2747d0759cbdf",
-    measurementId: "G-8Y55DXE8C4"
+        apiKey: "AIzaSyArYCA14-VbksnUswWrqMmdPs5qgNxhn3I",
+        authDomain: "mflix-bbcee.firebaseapp.com",
+        projectId: "mflix-bbcee",
+        storageBucket: "mflix-bbcee.appspot.com",
+        messagingSenderId: "519775413902",
+        appId: "1:519775413902:web:f2128094e2747d0759cbdf",
+        measurementId: "G-8Y55DXE8C4"
 };
     // Initialize Firebase
-   /* firebase.initializeApp(firebaseConfig);
-    firebase.analytics();*/
-/*$(function () {
+    /*firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+$(function () {
     firebase.initializeApp(firebaseConfig);
     alert(firebase.SDK_VERSION)
 });*/
